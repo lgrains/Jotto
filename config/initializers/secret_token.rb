33a4205b29089bd48ab1ce487f60424a45072964
Louise Rains <lrains@berklee.edu>
2012-03-01 21:05:22 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jotto::Application.config.secret_token = 'dbf6bc800addbc255d36da95f8b0b4f0d02bbae6178aa570f155a9571f2c2d1aa97554515c8b9e5955728a21e00a7e8928262c1a4616c9b3a68337d6c7095a2a'
